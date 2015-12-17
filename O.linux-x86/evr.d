0088b5985776934657864ce73b42358096d9719e
Abdallah Ismail <abdallah.ismail@sesame.org.jo>
2015-12-17 14:57:10 +0200

    Removed unnecessary files.

--- O.linux-x86/evr.d
+++
@@ -1,7 +0,0 @@
-evr.o: ../evr.c \
- /home/abdallah/project/ioc/epics/base-3.14.12.3/include/epicsExport.h \
- /home/abdallah/project/ioc/epics/base-3.14.12.3/include/shareLib.h \
- /home/abdallah/project/ioc/epics/base-3.14.12.3/include/drvSup.h \
- /home/abdallah/project/ioc/epics/base-3.14.12.3/include/errMdef.h \
- /home/abdallah/project/ioc/epics/base-3.14.12.3/include/shareLib.h \
- /home/abdallah/project/ioc/epics/base-3.14.12.3/include/iocsh.h ../evr.h
